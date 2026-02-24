0f243e80db2bf566172aca7309d4676748359562
j <73038451+kedachii@users.noreply.github.com>
2026-02-24 12:28:14 +0000

    added function

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,14 @@ import sqlite3
 conn = sqlite3.connect("schema.sql")
 cursor = conn.cursor()
 
-API_LINK = ""
-API_KEY = ""
+API_LINK = "https://v3.football.api-sports.io/"
+API_KEY = "7f14422097825f6406284820ff8f58cc"
 
 def get(request) {
     headers = {
         "x-apisports-key": API_KEY,
     }
+
     try {
         response = requests.request("GET", API_LINK, headers=headers)
         response.raise_for_status()
@@ -19,7 +20,7 @@ def get(request) {
     }
 }
 
-def manage_table(data) {
+def process_data(data) {
 
 }
 

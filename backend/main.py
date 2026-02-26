fc12c15a0869ac4faf45f045bbc73eeb5765e131
jacob <73038451+kedachii@users.noreply.github.com>
2026-02-25 21:02:57 -0800

    added json parsing with api

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,75 @@
 import requests
 import sqlite3
+from pathlib import Path
 
-conn = sqlite3.connect("schema.sql")
+from dummy import mock_responses
+
+import responses
+
+current_file = Path(__file__).resolve()
+schema_path = current_file.parent.parent / "database" / "schema.sql"
+db_path = current_file.parent.parent / "database" / "database.db"
+
+conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
-API_LINK = "https://v3.football.api-sports.io/"
+#API_LINK = "https://v3.football.api-sports.io/"
 API_KEY = "7f14422097825f6406284820ff8f58cc"
+API_LINK = "http://example.com"
 
-def get(request) {
+mock_responses.register_mocks()
+@responses.activate
+def get(request):
     headers = {
         "x-apisports-key": API_KEY,
     }
-
-    try {
-        response = requests.request("GET", API_LINK, headers=headers)
+    try:
+        response = requests.request("GET", API_LINK + request, headers=headers)
         response.raise_for_status()
         data = response.json()
-        print(data)
-    }
-}
+        return data
+    except Exception as e:
+        print(e)
 
-def process_data(data) {
 
-}
+def process_league(data):
+    for i1, data in enumerate(data["response"]):
+        leagueid = data["league"]["id"]
+        seasons = data["seasons"]
+        if data["country"]["name"] == "England" and leagueid < 42:
+            for i2, season in enumerate(seasons):
+                Name = data["league"]["name"]
+                SeasonID = season["year"]
+                StartDate = season["start"]
+                EndDate = season["end"]
+                print(Name)
+                print(SeasonID)
+
+def init_db():
+    try:
+        with open(schema_path, 'r') as f: # reads schema
+            sql_script = f.read()
+        
+        cursor.executescript(sql_script)
+        conn.commit() # saves
+        print("Database initialised")
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
 
 if __name__ == "__main__":
-    pass
+    try: # check if db has been initialised
+        query = "SELECT * FROM Player" 
+        cursor.execute(query) 
+        print(cursor.fetchall())
+    except Exception as e: # if not, initialise db
+        print(e)
+        init_db()
+
+    print("data:")
+    league_data = get("/leagues")
+    process_league(league_data)
+    
+    

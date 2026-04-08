6a2832df395949ece10695b1c078fd2f775391f1
Ziad Azmi <sgaazmi@liverpool.ac.uk>
2026-04-08 13:48:15 +0800

    flask templates

--- backend/routes/fixtures.py
+++ backend/routes/fixtures.py
@@ -1,1 +1,14 @@
+from flask import Blueprint, render_template
 
+# create blueprint
+fixtures = Blueprint("fixtures", __name__)
+
+# all fixtures page
+@fixtures.route("/fixtures")
+def show_fixtures():
+    return render_template("live.html")
+
+# single match page
+@fixtures.route("/match/<int:id>")
+def match(id):
+    return render_template("match.html", match_id=id)
--- backend/routes/main.py
+++ backend/routes/main.py
@@ -1,1 +1,14 @@
+from flask import Blueprint, render_template
 
+# create blueprint
+main = Blueprint("main", __name__)
+
+# homepage
+@main.route("/")
+def home():
+    return render_template("home.html")
+
+# live matches page
+@main.route("/live")
+def live():
+    return render_template("live.html")

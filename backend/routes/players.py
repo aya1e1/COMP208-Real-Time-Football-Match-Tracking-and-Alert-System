20da05e948f8e174001ef1d27c6107b15441356a
balqishalim <139963415+balqishalim@users.noreply.github.com>
2026-04-15 13:07:26 +0100

    Add Flask blueprint for displaying football teams

--- backend/routes/players.py
+++ backend/routes/players.py
@@ -1,1 +1,9 @@
+from flask import Blueprint, render_template
 
+# create blueprint
+teams = Blueprint("teams", __name__)
+
+# display all football teams
+@teams.route("/teams")
+def show_teams():
+  return render_template("teams.html")

5b77e86803ae740b13b425c90acfc6ae0e948e47
Thomas-Sutton-0 <tomsutton002@gmail.com>
2026-04-18 21:06:24 +0100

    Updated notifier to add notify function
    also added the shorter get_pending_notifications function

--- backend/events/notifier.py
+++ backend/events/notifier.py
@@ -2,19 +2,45 @@
 from backend.db.database import query
 
 _notifications = []
-
-
 class Notifier:
-
     def notify(self, fixture_id, team_id, event_type, detail, minute):
-        notification = {
-            "fixture_id" : fixture_id,
-            "team_id" : team_id,
-            "event_type" : event_type,
-            "detail" : detail,
-            "minute" : minute
-        }
-        
-        _notifications.append(notification)
-        
-        print(f"[{minute}'] {event_type} - {detail} (Team {team_id})")
+        userList = []
+        if event_type=="Goal":
+            userList = query("SELECT DISTINCT t.UserID FROM UserFavouriteTeams t JOIN UserNotificationPreferences p ON t.UserID = p.UserID AND t.TeamID = p.TeamID WHERE t.TeamID = ? AND p.NotifyGoals = TRUE", team_id)
+            for row in userList:
+                _notifications.append({
+                    "UserID": row["UserID"],
+                    "TeamID": team_id,
+                    "message": "GOAL",
+                    "detail": detail,
+                    "minute": minute
+                })
+
+        elif event_type=="Card":
+            userList = query("SELECT DISTINCT t.UserID FROM UserFavouriteTeams t JOIN UserNotificationPreferences p ON t.UserID = p.UserID AND t.TeamID = p.TeamID WHERE t.TeamID = ? AND p.NotifyCards = TRUE", team_id)
+            for row in userList:
+                _notifications.append({
+                    "UserID": row["UserID"],
+                    "TeamID": team_id,
+                    "message": "CARD",
+                    "detail": detail,
+                    "minute": minute
+                })
+
+        elif event_type=="Substitution":
+            userList = query("SELECT DISTINCT t.UserID FROM UserFavouriteTeams t JOIN UserNotificationPreferences p ON t.UserID = p.UserID AND t.TeamID = p.TeamID WHERE t.TeamID = ? AND p.NotifySubstitutions = TRUE", team_id)
+            for row in userList:
+                _notifications.append({
+                    "UserID": row["UserID"],
+                    "TeamID": team_id,
+                    "message": "SUBSTITUTION",
+                    "detail": detail,
+                    "minute": minute
+                })
+
+
+def get_pending_notifications():
+    global _notifications
+    notifs = _notifications
+    _notifications = []
+    return notifs

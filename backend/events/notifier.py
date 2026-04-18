28e2155c40bd0f48a7e0009270e285c24956e9c4
Hamza Al Zudi Garcia-Olalla <hamzaalzudi@gmail.com>
2026-04-18 18:07:16 +0100

    Implemented notifications to printed for testing system

--- backend/events/notifier.py
+++ backend/events/notifier.py
@@ -1,5 +1,5 @@
 """notifier.py - User notification engine."""
-from backend.db.batabase import query
+from backend.db.database import query
 
 _notifications = []
 
@@ -7,3 +7,14 @@ _notifications = []
 class Notifier:
 
     def notify(self, fixture_id, team_id, event_type, detail, minute):
+        notification = {
+            "fixture_id" : fixture_id,
+            "team_id" : team_id,
+            "event_type" : event_type,
+            "detail" : detail,
+            "minute" : minute
+        }
+        
+        _notifications.append(notification)
+        
+        print(f"[{minute}'] {event_type} - {detail} (Team {team_id})")
